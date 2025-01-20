1fade5ba66a8029492d3e9b4282a04a705b30a69
Алексей <sayhell042@gmail.com>
2025-01-20 08:41:52 +0700

    homework-19-01-25

---
+++ src/main/java/org/example/order.sql
@@ -0,0 +1,6 @@
+CREATE TABLE orders(
+    "id"          BIGINT,
+    "customer_id" BIGINT,
+    "product_id"  BIGINT,
+    "type"        VARCHAR(30)
+);
---
+++ src/main/java/org/example/product.sql
@@ -0,0 +1,6 @@
+CREATE TABLE product(
+    "id"    BIGINT,
+    "name"  VARCHAR(200),
+    "price" DOUBLE PRECISION,
+    "type"  VARCHAR(30)
+);
